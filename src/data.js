620a24fcfc51aef04ff0020324b25e099e2ab768
Juan Baranovsky <xxbaraxx@hotmail.com>
2023-04-28 19:04:01 -0300

    add styled-componentens in wordle app

--- src/data.js
+++ src/data.js
@@ -13,7 +13,7 @@ export const PROJECTS = [
     description:
       "Copy of Wordle Game, the viral game in 2022. Wordle is a word-guessing game in which players must guess a five-letter word chosen by the game's algorithm. Each guess is evaluated and players receive feedback indicating how many letters in their guess are correct and in the correct position. Players have six attempts to correctly guess the word. ",
     image: '/wordleImg.png',
-    techs: ['React'],
+    techs: ['React', 'Styled-Components'],
     githubCode: 'https://github.com/Bara1422/WordleApp',
     demoPage: 'https://wordle-app-bara1422.vercel.app/'
   },

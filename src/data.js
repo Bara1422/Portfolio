31e902c9d66446e8967795fb52a710db21522bae
Juan Baranovsky <xxbaraxx@hotmail.com>
2023-05-01 17:13:47 -0300

    add e-commerce project

--- src/data.js
+++ src/data.js
@@ -25,5 +25,22 @@ export const PROJECTS = [
     techs: ['React', 'Tailwind'],
     githubCode: 'https://github.com/Bara1422/WeatherApp',
     demoPage: 'https://weather-app-bara1422.vercel.app/'
+  },
+  {
+    name: 'E-commerce',
+    description:
+      'E-commerce integrated with Backend make in Node+Express, Mercado Pago API, JWT, Redux, Styled-Components, Axios, React-Query.',
+    image: '/E-Commerce.png',
+    techs: [
+      'React',
+      'Styled-Components',
+      'Mercado Pago',
+      'Redux',
+      'JWT',
+      'React-Query'
+    ],
+    githubCode:
+      'https://github.com/Bara1422/frontend-integrador/tree/node-integrate',
+    demoPage: 'https://tod-gaming-reactintegrador-bara1422.vercel.app/'
   }
 ]

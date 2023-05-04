925de7085e99e543b4d5324f3b5e85ed50083192
Juan Baranovsky <xxbaraxx@hotmail.com>
2023-05-04 16:53:43 -0300

    change projects description2

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const PROJECTS = [
   {
     name: 'Freectionary',
     description:
-      'An English dictionary app that uses a https://dictionaryapi.dev API to provide accurate definitions and translations of words.',
+      'An English dictionary app that uses a https://dictionaryapi.dev API to provide accurate definitions and pronunciations of words.',
     image: '/freectionaryImg.png',
     techs: ['React', 'Tailwind'],
     githubCode: 'https://github.com/Bara1422/Freectionary',
@@ -11,7 +11,7 @@ export const PROJECTS = [
   {
     name: 'Wordle Game',
     description:
-      "Copy of Wordle Game, the viral game in 2022. Wordle is a word-guessing game in which players must guess a five-letter word chosen by the game's algorithm. Each guess is evaluated and players receive feedback indicating how many letters in their guess are correct and in the correct position. Players have six attempts to correctly guess the word. ",
+      "Copy of Wordle Game, the 2022 viral game. Wordle is a word-guessing game in which players must guess a five-letter word chosen by the game's algorithm. Each guess is evaluated and players receive feedback indicating how many letters in their guess are correct and in the correct position. Players have six attempts to correctly guess the word. ",
     image: '/wordleImg.png',
     techs: ['React', 'Styled-Components'],
     githubCode: 'https://github.com/Bara1422/WordleApp',
@@ -20,7 +20,7 @@ export const PROJECTS = [
   {
     name: 'Weather App',
     description:
-      'A sleek and easy-to-use app that provides weather information for the current day and next two days, as well as hourly data. The background images change according to the actual weather, making it visually appealing. ',
+      'An easy-to-use app that provides weather information for the current day and next two days, as well as hourly data. The background images change according to the weather, making it visually appealing. ',
     image: '/weatherapp.png',
     techs: ['React', 'Tailwind'],
     githubCode: 'https://github.com/Bara1422/WeatherApp',
